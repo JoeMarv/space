a6baef650bd6ee87ac1cda161dab186a43aed7da
Joel <joepotential@gmail.com>
2022-08-07 07:53:35 +0100

    first comit

---
+++ app.js
@@ -0,0 +1,17 @@
+const explore = document.querySelector('.explore-btn button')
+const menu = document.querySelector('.menu')
+const menuBtn = document.querySelector('.menu-btn button')
+const closeBtn = document.querySelector('.close-btn button')
+
+
+explore.addEventListener('click', function() {
+    location.href = 'destination-moon.html'
+})
+
+menuBtn.addEventListener('click', function() {
+    menu.classList.add('open')
+})
+
+closeBtn.addEventListener('click', function() {
+    menu.classList.remove('open')
+})
